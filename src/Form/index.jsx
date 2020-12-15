3d5b236b8fdfa3615cef8af46ad0a24e0555e805
Beicker Zambrano <beicker998@gmail.com>
2020-12-15 17:40:39 -0500

    Add labels and a submit button to the Form
    Add a label of styled-components in the Input
    of styles.jsx. Also create an Submit component
    with only a margin style to be the submit input
    of the form

--- src/Form/index.jsx
+++ src/Form/index.jsx
@@ -1,14 +1,15 @@
-import { Wrapper, Input, FormElement } from "./styles.jsx";
+import { Submit, Wrapper, Input, FormElement } from "./styles.jsx";
 export default function Form() {
   return (
     <Wrapper>
       <FormElement>
-        <Input type="text" placeholder="Type Here" />
-        <Input type="text" placeholder="Type Here" />
-        <Input type="text" placeholder="Type Here" />
-        <Input type="text" placeholder="Type Here" />
-        <Input type="text" placeholder="Type Here" />
-        <Input type="text" placeholder="Type Here" />
+        <Input label="Nombre" type="text" placeholder="Type Here" />
+        <Input label="Número de Teléfono" type="text" placeholder="Type Here" />
+        <Input label="Usuario" type="text" placeholder="Type Here" />
+        <Input label="Apellido" type="text" placeholder="Type Here" />
+        <Input label="Correo Electrónico" type="text" placeholder="Type Here" />
+        <Input label="Contraseña" type="text" placeholder="Type Here" />
+        <Submit type="submit" value="REGISTRATE" />
       </FormElement>
     </Wrapper>
   );
--- src/Form/styles.jsx
+++ src/Form/styles.jsx
@@ -11,16 +11,28 @@ export const FormElement = styled.form`
   width: 100%;
 `;
 
-const InputElement = styled.input``;
+const Label = styled.label`
+  font-weight: bold;
+`;
+
+const InputElement = styled.input`
+  margin-top: 0.5rem;
+`;
+
+export const Submit = styled.input`
+  margin-top: 2rem;
+  cursor: pointer;
+`;
 
 const InputWrapper = styled.div`
   max-width: 50%;
   margin-top: 1rem;
 `;
 
-export const Input = ({ type, placeholder }) => {
+export const Input = ({ type, placeholder, label }) => {
   return (
     <InputWrapper>
+      <Label>{label}</Label>
       <InputElement type={type} placeholder={placeholder} />
     </InputWrapper>
   );
